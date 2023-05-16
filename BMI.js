197c0a32d3471eccb3ee018daed4b5d0c72a1be0
bisinle <abdiwadud.mohamedd@gmail.comm>
2023-05-16 23:29:56 +0300

    fixed

--- BMI.js
+++ BMI.js
@@ -7,14 +7,14 @@ header.innerText = 'BMI CALCULATOR'
 BmiDIV.append(header)
 header.classList.add('h1')
 
-const heightInput = document.createElement('input')
 const weightInput = document.createElement('input')
-heightInput.setAttribute('type', 'text')
-weightInput.setAttribute('type', 'text')
+const heightInput = document.createElement('input')
+heightInput.setAttribute('type', 'number')
+weightInput.setAttribute('type', 'number')
 heightInput.min =0
 weightInput.min =0
-heightInput.setAttribute('placeholder', 'Enter  Height in meters')
 weightInput.setAttribute('placeholder', 'Enter  Weights in kgs')
+heightInput.setAttribute('placeholder', 'Enter  Height in meters')
 heightInput.classList.add('input')
 weightInput.classList.add('input')
 BmiDIV.append(heightInput)
@@ -26,15 +26,21 @@ btn.innerText = 'Calculate'
 btn.classList.add('btn')
 BmiDIV.append(btn)
 
-btn.addEventListener('click', function (){
-  let EnteredHeight = heightInput.value
-  let EnteredWeight = weightInput.value
-
- let BMI = Math.floor((EnteredWeight)/(Math.pow(EnteredHeight,2)))
-
- let comment = document.createElement('p')
- comment.textContent =`Your BMI is${BMI} `
+let comment = document.createElement('p')
+comment.classList.add('p')
 BmiDIV.append(comment)
 
 
+btn.addEventListener('click', function (){
+  let EnteredHeight = heightInput.value
+  let EnteredWeight = weightInput.value
+  let BMI = (EnteredWeight)/(Math.pow(EnteredHeight,2))
+  let RoundedBMI = Math.round(BMI) 
+  comment.textContent =`Your BMI is ${RoundedBMI}`  
+  heightInput.value = '' 
+  weightInput.value = ''
+
+
 })
+
+
